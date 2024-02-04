bd898fce8c52d3677fa1208c3efc061db3f14f1b
Ghabryel Henrique <ghabryelhenrique@hotmail.com>
2024-02-04 08:11:01 -0300

    Initial Commit

---
+++ src/environment/environment.ts
@@ -0,0 +1,31 @@
+export const environment = {
+  production: false,
+  PIX_CODE: '00020126580014BR.GOV.BCB.PIX0136a1824ded-175f-4ee9-906b-ca4d8ab43b175204000053039865802BR5925Ghabryel Henrique Ferreir6009SAO PAULO61080540900062070503***6304D0A4',
+  PANDASCORE: {
+    BASE_URL: 'https://api.pandascore.co',
+    TOKEN_ACCESS: 'Bearer rytahi7kZGVO9zpWHTT3cg7sBaifUKg_A8Pgb-pD0bpoMbcsTwc',
+  },
+  LOLESPORTSAPI: {
+    ESPORTS_API: 'https://esports-api.lolesports.com/persisted/gw',
+    FEEDLOLAPI: 'https://feed.lolesports.com/livestats/v1',
+    API_LOLESPORTS: 'https://api.lolesports.com/api/v1',
+    TOKEN_ACCESS: '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z',
+  },
+  VALORANTESPORTSAPI: {
+    ESPORTS_API:
+      'https://esports-api.service.valorantesports.com/persisted/val',
+    TOKEN_ACCESS: '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z',
+  },
+  RAPIDA_API: {
+    RAPIDA_API_HOST: 'https://valorant-esports.p.rapidapi.com',
+    RAPIDA_API_TOKEN: '2e913d6562msh0a17adba110541bp183c9djsn667449c6eb50',
+  },
+  CHAMPIONS_URL:
+    'https://ddragon.leagueoflegends.com/cdn/12.14.1/img/champion/',
+  ITEMS_URL : "https://ddragon.bangingheads.net/cdn/latest/img/item/",
+  PLAYERS_STREAM: {
+    TWITCH_URL_STREAM: 'https://player.twitch.tv/',
+    YOUTUBE_URL_VOD: 'https://www.youtube.com/embed',
+    TROVO_URL_VOD: 'https://player.trovo.live/embed/player',
+  }
+};
